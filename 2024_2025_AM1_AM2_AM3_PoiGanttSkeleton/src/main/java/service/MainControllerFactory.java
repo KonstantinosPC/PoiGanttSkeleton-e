5c70c661fcacd861f980032bfb6b5dcb8c0397db
iliopoulos730@gmail.com <80127970+KonstantinosPC@users.noreply.github.com>
2024-11-10 23:30:14 +0200

    Uploading Files

---
+++ 2024_2025_AM1_AM2_AM3_PoiGanttSkeleton/src/main/java/service/MainControllerFactory.java
@@ -0,0 +1,9 @@
+package service;
+
+public class MainControllerFactory {
+
+	public IMainController createMainController() {
+		return null;
+		//TODO FIX: return a new instance of your IMainController implementation ;
+	}
+}
